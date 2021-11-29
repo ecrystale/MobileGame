3a3d90dc6064855404dc66dd7f9990ecec931d5f
ecrystale <el2679@nyu.edu>
2021-11-28 19:43:55 -0500

    adding enemies by file

---
+++ Assets/Scripts/Enemy1.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Enemy1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] enemy;
+    public float interval = 2f;
+    public string filename;
+
+    private string[] spawn;
+    private float origint;
+    private int line;
+
+    void Start()
+    {
+        spawn = System.IO.File.ReadAllLines("Assets/Scripts/Stages/"+filename);
+        /*foreach (string lines in spawn){
+          print(lines);
+        }*/
+        origint = interval;
+        line = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     interval -= Time.deltaTime;
+     if (interval < 0)
+     {
+        int offset = 0;
+        foreach(char part in spawn[line]){
+           if (Char.IsDigit(part)){
+             Vector3 pos = transform.position;
+             pos.x+=offset;
+             GameObject enem = Instantiate(enemy[(int)Char.GetNumericValue(part)],pos, transform.rotation);
+            //enem.velocity = transform.TransformDirection(Vector3.forward * 10);
+           }
+           offset+=3;
+         }
+         line+=1;
+         interval = origint;
+     }
+
+    }
+}
