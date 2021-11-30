e5256fe277edcf9f5c2ecbc7e5c75dd899c3590f
Johnson Li <johnson.li61001@gmail.com>
2021-11-29 20:24:31 -0500

    clamp player pos

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,13 +16,20 @@ public class PlayerMovement : MonoBehaviour{
                 initialPos = Camera.main.ScreenToWorldPoint(touch.position);
                 xOffset = initialPos.x - transform.position.x;
                 yOffset = initialPos.y - transform.position.y;
+                print(Camera.main.ScreenToWorldPoint(touch.position));
+                print(Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0f)));
             }
 
             if(touch.phase == TouchPhase.Moved){
                 float newX = Camera.main.ScreenToWorldPoint(touch.position).x - xOffset;
                 float newY = Camera.main.ScreenToWorldPoint(touch.position).y - yOffset;
+
+                float upperXBound = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0f)).x;
+                float lowerXBound = -upperXBound;
+                float lowerYBound = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0f)).y;
+                float upperYBound = -lowerYBound;
                 
-                transform.position = new Vector2(Mathf.Clamp(newX, -2.5f, 2.5f), Mathf.Clamp(newY, -4.6f, 4.6f));
+                transform.position = new Vector2(Mathf.Clamp(newX, lowerXBound, upperXBound), Mathf.Clamp(newY, lowerYBound, upperYBound));
                 
             }
         }
