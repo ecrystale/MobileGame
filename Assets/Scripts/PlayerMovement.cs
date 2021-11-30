fca183047f210acde3939fb3891ef2e12c051dbc
Johnson Li <johnson.li61001@gmail.com>
2021-11-29 22:27:38 -0500

    moved bound calculation for player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,10 @@ public class PlayerMovement : MonoBehaviour{
     private Vector2 initialPos;
     private float xOffset;
     private float yOffset;
+    private float upperXBound = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0f)).x;
+    private float lowerXBound = -Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0f)).x;
+    private float lowerYBound = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0f)).y;
+    private float upperYBound = -Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0f)).y;
 
     void Update(){
         if(Input.touchCount > 0){
@@ -21,11 +25,6 @@ public class PlayerMovement : MonoBehaviour{
             if(touch.phase == TouchPhase.Moved){
                 float newX = Camera.main.ScreenToWorldPoint(touch.position).x - xOffset;
                 float newY = Camera.main.ScreenToWorldPoint(touch.position).y - yOffset;
-
-                float upperXBound = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0f)).x;
-                float lowerXBound = -upperXBound;
-                float lowerYBound = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0f)).y;
-                float upperYBound = -lowerYBound;
                 
                 transform.position = new Vector2(Mathf.Clamp(newX, lowerXBound, upperXBound), Mathf.Clamp(newY, lowerYBound, upperYBound));
                 
