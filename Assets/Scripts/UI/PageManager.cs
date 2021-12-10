168e7a5512effb8c9e0c1cbe3db7fd0f4c8f4919
PIG208 <359101898@qq.com>
2021-12-09 23:16:07 -0500

    Add levels page

--- Assets/Scripts/UI/PageManager.cs
+++ Assets/Scripts/UI/PageManager.cs
@@ -5,6 +5,7 @@ public class PageManager : MonoBehaviour
 {
     public MenuManager menu;
     public bool Showed;
+    public GameObject[] PassiveObjects;
 
     public event Action<PageManager> PageShowed;
     public event Action<PageManager> PageHid;
@@ -23,12 +24,20 @@ public class PageManager : MonoBehaviour
     public void ShowPage()
     {
         Showed = true;
+        foreach (GameObject gameObject in PassiveObjects)
+        {
+            gameObject.SetActive(true);
+        }
         if (PageShowed != null) PageShowed(this);
     }
 
     public void HidePage()
     {
         Showed = false;
+        foreach (GameObject gameObject in PassiveObjects)
+        {
+            gameObject.SetActive(false);
+        }
         if (PageShowed != null) PageHid(this);
     }
 
