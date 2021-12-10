6b1118fbd9b9bbe8531fa44e7dd615f69cc60ef3
PIG208 <359101898@qq.com>
2021-12-10 13:44:52 -0500

    Add expand UIComponent

---
+++ Assets/Scripts/UI/Expand.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Expand : UIComponent
+{
+    private Vector3 StartScale = Vector3.zero;
+    public float duration;
+
+    private Vector3 _startScale;
+    private Vector3 _endScale;
+    private Vector3 _destScale;
+    private float _speed;
+
+    protected override void Start()
+    {
+        base.Start();
+        _endScale = transform.localScale;
+        _speed = 1 / duration;
+
+        _destScale = _endScale;
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, _destScale, _speed * Time.deltaTime);
+    }
+
+    public override void HandlePageShowed(PageManager page)
+    {
+        _destScale = _endScale;
+    }
+
+    public override void HandlePageHid(PageManager page)
+    {
+        _destScale = _startScale;
+    }
+}
--- Assets/Scripts/UI/UIComponent.cs
+++ Assets/Scripts/UI/UIComponent.cs
@@ -1,13 +1,35 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class UIComponent : MonoBehaviour
 {
+    public float EnterDelay;
+    public float ExitDelay;
     public PageManager page;
 
     protected virtual void Start()
     {
-        page.PageShowed += HandlePageShowed;
-        page.PageHid += HandlePageHid;
+        Register();
+    }
+
+    public void Register()
+    {
+        if (page == null) return;
+        page.PageShowed += (PageManager page) => DelayedExecute(EnterDelay, () => HandlePageShowed(page));
+        page.PageHid += (PageManager page) => DelayedExecute(ExitDelay, () => HandlePageHid(page));
+    }
+
+    protected void DelayedExecute(float delay, Action callback)
+    {
+        if (delay == 0) callback();
+        else StartCoroutine(HandleDelayExeuction(delay, callback));
+    }
+
+    private IEnumerator<WaitForSeconds> HandleDelayExeuction(float delay, Action callback)
+    {
+        yield return new WaitForSeconds(delay);
+        callback();
     }
 
     public abstract void HandlePageShowed(PageManager page);
